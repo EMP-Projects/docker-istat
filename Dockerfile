630abc8eb7d0508d5a6e5573e20d33c1054444a2
Giuseppe Zileni <giuseppe.zileni@gmail.com>
2024-11-25 17:28:45 +0100

    update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,35 @@
-FROM osgeo/gdal:alpine-small-3.6.3
+# Use the official Debian image as a base
+FROM ubuntu:22.04
 
-WORKDIR /root/geojson
+# Set environment variables
+ENV CPLUS_INCLUDE_PATH=/usr/include/gdal
+ENV C_INCLUDE_PATH=/usr/include/gdal
+
+# Install required packages
+RUN apt-get update && \
+    apt-get install -y software-properties-common && \
+    add-apt-repository ppa:ubuntugis/ppa && \
+    apt-get update && \
+    apt-get install -y \
+        gdal-bin \
+        python3 \
+        python3-venv \
+        python3-pip \
+        python3-dev \
+        libpq-dev && \
+    rm -rf /var/lib/apt/lists/*
+
+# Install Python packages
+RUN pip3 install \
+    GDAL \
+    osmium \
+    psycopg2 \
+    numpy
+
+WORKDIR /istat
 
 COPY ./geojson/limits_IT_regions.geojson .
 COPY ./geojson/limits_IT_provinces.geojson .
 COPY ./geojson/limits_IT_municipalities.geojson .
-
-WORKDIR /root
 COPY ./scripts/istat-import.sh .
-RUN chmod +x ~/istat-import.sh
-
-ENTRYPOINT ["~/istat-import.sh"]
+RUN chmod +x /istat/istat-import.sh
--- scripts/istat-import.sh
+++ scripts/istat-import.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
 # se la cartella geojson non esiste la crea
-if [ ! -d "geojson" ]
+if [ ! -d "istat" ]
 then
-    echo "Non trovo la cartella geojson"
+    echo "Non trovo la cartella istat"
+    exit
+fi
+
+# legge tutti i file .geojson dalla cartella istat
+shopt -s nullglob
+files=(istat/*.geojson)
+
+# verifica se ci sono file .geojson nella cartella
+if [ ${#files[@]} -eq 0 ]; then
+    echo "Non ci sono file .geojson nella cartella istat"
     exit
 fi
 
 # legge tutti i file .geojson e li importa nel database
-for file in geojson/*.geojson
-do
+for file in "${files[@]}"; do
     # estrae il nome del file
-    table=$(basename $file .geojson)
+    table=$(basename "$file" .geojson)
     
-    ogr2ogr -nlt PROMOTE_TO_MULTI -nln $table PG:"host=$PGHOST port=$PGPORT user=$PGUSER password=$PGPWD dbname=$PGDB" $file -f "PostgreSQL" -overwrite -s_srs EPSG:4326 -t_srs EPSG:3857 
-    
-    # stampa a video il nome del file importato
-    echo "Importato il file $file"
+    if ogr2ogr -nlt PROMOTE_TO_MULTI -nln "$table" PG:"host=$PGHOST port=$PGPORT user=$PGUSER password=$PGPWD dbname=$PGDB" "$file" -f "PostgreSQL" -overwrite -s_srs EPSG:4326 -t_srs EPSG:3857; then
+        # stampa a video il nome del file importato
+        echo "Importato il file $file"
+    else
+        echo "Errore durante l'importazione del file $file"
+        exit 1
+    fi
 done
